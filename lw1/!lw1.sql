f722e2125776292ba44f82e8154d1317d928a5dc
eligantRU <eligant.ru@gmail.com>
2017-03-27 20:45:21 +0400

    Fixed the 1st lab

--- lw1/!lw1.sql
+++ lw1/!lw1.sql
@@ -3,11 +3,22 @@ CREATE DATABASE rental;
 USE rental;
 
 CREATE TABLE
+  human
+(
+  id INT UNSIGNED NOT nULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  passport_code VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) UNIQUE,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE
   customer
 (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255),
-  passport_code VARCHAR(255), 
+  registration_date DATE NOT NULL,
+  human_id INT UNSIGNED NOT NULL UNIQUE,
+  FOREIGN KEY(human_id) REFERENCES human(id),
   PRIMARY KEY(id)
 );
 
@@ -15,16 +26,9 @@ CREATE TABLE
   owner
 (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255),
-  PRIMARY KEY(id)
-);
-       
-CREATE TABLE
-  category
-(
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  limitation_id INT UNSIGNED NOT NULL,
-  name VARCHAR(255) NOT NULL,
+  human_id INT UNSIGNED NOT NULL UNIQUE,
+  rating TINYINT UNSIGNED,
+  FOREIGN KEY(human_id) REFERENCES human(id),
   PRIMARY KEY(id)
 );
 
@@ -32,16 +36,26 @@ CREATE TABLE
   limitation
 (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  label VARCHAR(255),
+  label VARCHAR(255) UNIQUE,
   PRIMARY KEY(id)
 );
-
+       
+CREATE TABLE
+  category
+(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL UNIQUE,    
+  limitation_id INT UNSIGNED,
+  PRIMARY KEY(id)
+);
+                           
 CREATE TABLE
   worker
 (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255),
-  passport_code VARCHAR(255),
+  position VARCHAR(255) NOT NULL,
+  human_id INT UNSIGNED NOT NULL UNIQUE,
+  FOREIGN KEY(human_id) REFERENCES human(id),
   PRIMARY KEY(id)
 );
 
@@ -49,8 +63,9 @@ CREATE TABLE
   black_list
 (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  bad_user_id INT UNSIGNED NOT NULL,
-  FOREIGN KEY(bad_user_id) REFERENCES customer(id),
+  starting_date DATE NOT NULL,
+  bad_human_id INT UNSIGNED NOT NULL UNIQUE,
+  FOREIGN KEY(bad_human_id) REFERENCES customer(id),
   PRIMARY KEY(id)
 );
 
@@ -69,7 +84,7 @@ CREATE TABLE
 );
 
 CREATE TABLE 
-  inventory
+  object
 (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   owner_id INT UNSIGNED NOT NULL,
@@ -79,43 +94,73 @@ CREATE TABLE
   PRIMARY KEY(id)
 );
 
-INSERT 
-  INTO 
-    worker
-  (name, passport_code)
+INSERT
+  INTO
+    human
+  (name, passport_code, email)
 VALUES
-  ('Nick', '88 12 108462'),
-  ('Dart', '00 00 000000'),
-  ('Sarah', '19 18 123456'),
-  ('Michael', '48 15 162342'),
-  ('Alex', '17 178951'),
-  ('Nick', '99 889989'),
-  ('Ivan', '84 562117'),
-  ('Helen', '51 341141');
+  ('Nick', '88 12 108462', 'eligant.ru@gmail.com'),
+  ('Andrew', '88 11 105931', 'q@gmail.com'),
+  ('Olga', '88 12 123456', 'w@ya.ru'),
+  ('Elena', '88 13 102984', 'e@ya.ru'),
+  ('Michael', '88 10 111462', 'm@gmail.com'),
+  ('Nigan', '88 11 998802', 'p@gmail.com'),
+  ('Helen', '88 12 191451', 'l@gmail.com'),
+  ('Tatyana', '88 14 102984', 't@ya.ru');
 
-INSERT 
-  INTO 
+INSERT
+  INTO
     owner
-  (name)
+  (human_id)
 VALUES
-  ('Nick'),
-  ('Dart'),
-  ('Sarah'),
-  ('Michael'),
-  ('Dirack'),
-  ('Elena'),
-  ('Alex'),
-  ('Rudolf');
+  (1),
+  (2),
+  (5),
+  (6),
+  (8);
+
+INSERT
+  INTO
+    worker
+  (human_id, position)
+VALUES
+  (1, 'director'),
+  (2, 'accountant'),
+  (4, 'consultant'),
+  (5, 'consultant');
+
+INSERT
+  INTO
+    customer
+  (human_id, registration_date)
+VALUES
+  (1, '2009-11-30'),
+  (2, '2009-11-30'),
+  (3, '2010-01-01'),
+  (4, '2010-02-16'),
+  (5, '2010-04-11'),
+  (7, '2010-07-16'),
+  (8, '2010-12-13');
+
+INSERT
+  INTO
+    black_list
+  (bad_human_id, starting_date)
+VALUES
+  (2, '2010-01-02'),
+  (4, '2010-03-03'),
+  (6, '2011-07-07'),
+  (7, '2010-08-01');
 
 INSERT
   INTO
     limitation
   (label)
 VALUES
-  ('18+'),
-  ('16+'),
-  ('14+'),
   ('12+'),
+  ('14+'),
+  ('16+'),
+  ('18+'),
   ('21+');
 
 INSERT
@@ -123,52 +168,29 @@ INSERT
     category
   (name, limitation_id)
 VALUES
-  ('weapon', 1),
-  ('tool', 2),
+  ('weapon', 4),
+  ('tool', 3),
   ('computer technology', NULL),
   ('household appliances', NULL),
   ('book', NULL),
   ('laptop', 3),
   ('video game', NULL),
   ('DVD-film', NULL);
-
+    
 INSERT
   INTO
-    inventory
+    object
   (owner_id, name, category_id)
 VALUES
   (1, 'Magnum 500', 1),
   (1, 'Mosin-Nagant', 1),
   (2, 'chainsaw', 2),
-  (3, 'monitor', 3),
-  (4, 'washing machine', 4),
-  (6, 'radio', 5),
-  (5, 'M16', 1),
-  (7, 'The art of the Programming: I', 6);
-
-INSERT
-  INTO
-    customer
-  (name, passport_code)
-VALUES
-  ('Anrew', '11 222222'),
-  ('Sergey', '22 111111'),
-  ('Olga', '77 444444'),
-  ('Samanta', '66 999999'),
-  ('Helen', '19 876159'),
-  ('Vladimir', '88 110001'),
-  ('Tatyana', '88 120808'),
-  ('Elena', '88 808021');
-
-INSERT
-  INTO
-    black_list
-  (bad_user_id)
-VALUES
-  (4),
-  (5),
-  (6);
-
+  (5, 'monitor', 3),
+  (3, 'washing machine', 4),
+  (3, 'radio', 5),
+  (1, 'M16', 1),
+  (4, 'The art of the Programming: I', 6);
+         
 INSERT
   INTO
     offer
@@ -179,6 +201,6 @@ VALUES
   (3, 3, 4, '2013-01-10', '2013-02-10'),
   (4, 2, 2, '2011-01-01', '2011-01-02'),
   (1, 4, 1, '2012-01-01', NULL),
-  (5, 6, 6, '2012-07-07', '2012-08-08'),
-  (6, 5, 5, '2013-01-01', NULL),
-  (7, 6, 7, '2013-01-02', NULL);
+  (5, 6, 2, '2012-07-07', '2012-08-08'),
+  (6, 5, 1, '2013-01-01', NULL),
+  (7, 6, 4, '2013-01-02', NULL);
